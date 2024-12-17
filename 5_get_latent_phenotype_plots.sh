2f609a0ccd578c0e60cd00da896a13a0a19ef777
amyfrancis97 <amyfrancis1997@hotmail.com>
2024-12-17 09:25:10 +0000

    adding scripts to generate latent phenotype data

---
+++ 5_get_latent_phenotype_plots.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+
+source config.sh
+
+# Arguments
+FILTER=$1        # Filter type, e.g., "all_mutants" or "single_epitope"
+NUM_DOUBLES=$2   # Number of doubles to sample
+NUM_TRIPLES=$3   # Number of triples to sample
+
+# Set directory paths
+SBATCH_SCRIPT="$SCRIPT_DIR/run_jobs.sh"
+
+# Max number of jobs to submit at a time
+MAX_JOBS=3
+
+# Validate inputs
+if [ "$FILTER" != "all_mutants" ] && [ "$FILTER" != "single_epitope" ]; then
+    echo "Invalid filter. Use 'all_mutants' or 'single_epitope'."
+    exit 1
+fi
+
+# Collect trainval files based on the filter
+trainval_files=(${DATA_DIR}/*${FILTER}*_trainval_df.txt)
+
+# Iterate over all trainval/test file pairs
+for trainval_file in "${trainval_files[@]}"; do
+    # Extract base name and locate the matching test file
+    base_name=$(basename "$trainval_file" "_trainval_df.txt")
+    test_file="${DATA_DIR}/${base_name}_test_df.txt"
+
+    if [ -f "$test_file" ]; then
+        echo "Submitting job for $base_name..."
+
+        # Submit the job using sbatch
+        sbatch $SBATCH_SCRIPT sampling_scripts/get_latent_phenotype_info.py  \
+            "$base_name" \
+            "sample_double_triples" \
+            "additive" \
+            "$trainval_file" \
+            "$test_file" \
+            "$NUM_DOUBLES" \
+            "$NUM_TRIPLES" \
+            "$OUTPUT_PATH"
+
+        # Check the number of running jobs and wait if MAX_JOBS is reached
+        while [ "$(squeue -u $USER | wc -l)" -ge $((MAX_JOBS + 1)) ]; do
+            echo "Max number of jobs ($MAX_JOBS) reached. Waiting..."
+            sleep 90  # Check again after 10 seconds
+        done
+    else
+        echo "No matching test file for $base_name. Skipping."
+    fi
+done
+
